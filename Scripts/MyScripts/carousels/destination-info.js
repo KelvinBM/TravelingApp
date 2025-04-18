b518b141928ff8d82dee72bde18204f1866d387c
KBelloMejia <93287024+KelvinBM@users.noreply.github.com>
2025-04-18 12:54:14 -0400

    began working on DestinationInfo View
    got the jquery and carousel working; began working on the DestinationInfo View

---
+++ Scripts/MyScripts/carousels/destination-info.js
@@ -0,0 +1,42 @@
+﻿// $(document).ready(function(){
+//     ...jquery...
+// });
+$(function () {
+    let slideIndex = -1;
+    let slides = $(".slides");
+    //showSlides();
+    showSlides();
+
+    function showSlides() {
+        ////////////////////////////////////////
+        // attempt 1
+        slides.hide();
+
+        slides.each(function (index) {
+            $(this).css("border", "3px solid black");
+            $(this).css({
+                width: "560px",
+                height: "500px",
+                padding: "0",
+                contentSizing: "cover"
+            });
+            $(this).children("img").css({
+                width: "560px",
+                height: "500"
+            });
+        });
+
+        slideIndex++;
+        if (slideIndex > slides.length)
+            slideIndex = 1;
+
+        //alert(slideIndex)
+        let currSlide = slides.eq(slideIndex - 1);
+
+        currSlide.css("display", "block");
+        //currSlide.click(function () {
+        //    showSlides();
+        //})
+        setTimeout(showSlides, 4000);
+    }
+});
